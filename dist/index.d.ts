9110c155ef19dc7067ab6a4571ada7d56cdb50f8
jenny <lyndle0890@gmail.com>
2017-10-28 19:58:56 -0700

    test

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { myEmail } from './email';
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var email_1 = require("./email");
-exports.myEmail = email_1.myEmail;
